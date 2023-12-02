c07038634464a2fd1ab050910c9b9f8e56a8eb90
Rene <hornak.rene@gmail.com>
2023-12-02 21:11:49 +0100

    snake is moving

--- main.c
+++ main.c
@@ -1,10 +1,23 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
 
 #define COLS 60
 #define ROWS 30
 
 int main() {
+	//hide cursor
+	printf("\e[?25L");
+
+	//switch to canonical mode, disable echo
+	struct termios oldt, newt;
+	tcgetattr(STDIN_FILENO, &oldt);
+	newt = oldt;
+	newt.c_lflag &= ~(ICANON | ECHO);
+	tcsetattr(STDIN_FILENO, TCSANOW, &newt);
+	
+	int x[1000], y[1000];
 	int quit = 0;
 	while(!quit) {
 		//render table
@@ -21,11 +34,72 @@ int main() {
 			}
 			printf("#\n");
 		}		
-		printf("#");	
 		printf("#");
 		for (int i = 0; i < COLS; i++) {
 			printf("#");
 		}
 		printf("#\n");
+
+		//move cursor back to top
+		printf("\e[%iA", ROWS + 2);
+
+		int head = 0, tail = 0;
+		x[head] = COLS / 2;
+		y[head] = ROWS / 2;
+		int gameover = 0;
+		int xdir = 1, ydir = 0;
+
+		while(!quit && !gameover) {
+			//clear snake tail
+			printf("\e[%iB\e[%iC ", y[tail] + 1, x[tail] + 1);
+			printf("\e[%iF", y[tail] + 1);
+
+			tail = (tail + 1) % 1000;
+			int newhead = (head + 1) % 1000;
+			x[newhead] = (x[head] + xdir + COLS) % COLS;
+			y[newhead] = (y[head] + ydir + ROWS) % ROWS;
+			head = newhead;
+
+			//draw head
+			printf("\e[%iB\e[%iCV", y[head] + 1, x[head] + 1);
+			printf("\e[%iF", y[head] + 1);
+			fflush(stdout);
+
+			usleep(5 * 1000000 / 60);
+
+			//read keyword
+			struct timeval tv;
+			fd_set fds;
+			tv.tv_sec = 0;
+			tv.tv_usec = 0;
+
+			FD_ZERO(&fds);
+			FD_SET(STDIN_FILENO, &fds);
+			select(STDIN_FILENO + 1, &fds, NULL, NULL, &tv);
+			if (FD_ISSET(STDIN_FILENO, &fds)) {
+				int ch = getchar();
+				if (ch == 27 || ch == 'q') {
+					quit = 1;
+				} else if (ch == 'a' && xdir != 1) {
+					xdir = -1;
+					ydir = 0;
+				} else if (ch == 'd' && xdir != -1) {
+					xdir = 1;
+					ydir = 0;
+				} else if (ch == 's' && ydir != -1) {
+					xdir = 0;
+					ydir = 1;
+				} else if (ch == 'w' && ydir != 1) {
+					xdir = 0;
+					ydir = -1;
+				}
+				
+			}	
+		}
 	}
+	//show cursor
+	printf("\e[?25h");
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
+	return 0;
 }
