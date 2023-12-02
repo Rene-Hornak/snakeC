803d4bd72ee31704c5872e99c41e1bc4abd33edf
Rene <hornak.rene@gmail.com>
2023-12-02 23:21:38 +0100

    game over

--- main.c
+++ main.c
@@ -64,7 +64,7 @@ int main() {
 				}
 				if (applex >= 0) {
 					//draw apple
-					printf("\e[%iB\e[%iCo", appley + 1, appley + 1);
+					printf("\e[%iB\e[%iCo", appley + 1, applex + 1);
 					printf("\e[%iF", appley + 1);
 				}
 			}
@@ -73,11 +73,25 @@ int main() {
 			printf("\e[%iB\e[%iC ", y[tail] + 1, x[tail] + 1);
 			printf("\e[%iF", y[tail] + 1);
 
-			tail = (tail + 1) % 1000;
+			//create snake tail
+			if (x[head] == applex && y[head] == appley) {
+				applex = -1;
+				printf("\a"); //Bell
+			} else {
+				tail = (tail + 1) % 1000;
+			}
+			
 			int newhead = (head + 1) % 1000;
 			x[newhead] = (x[head] + xdir + COLS) % COLS;
 			y[newhead] = (y[head] + ydir + ROWS) % ROWS;
 			head = newhead;
+
+			//colision
+			for (int i = tail; i != head; i = (i + 1) % 1000) {
+				if (x[i] == x[head] && y[i] == y[head]) {
+					gameover = 1;
+				}
+			}
 
 			//draw head
 			printf("\e[%iB\e[%iCV", y[head] + 1, x[head] + 1);
@@ -112,8 +126,15 @@ int main() {
 					xdir = 0;
 					ydir = -1;
 				}
-				
 			}	
+		}
+
+		if (!quit) {
+			//show game over
+			printf("\e[%iB\e[%iC Game Over!", ROWS / 2, COLS / 2 - 5);
+			printf("\e[%iF", ROWS / 2);
+			fflush(stdout);
+			getchar();
 		}
 	}
 	//show cursor
